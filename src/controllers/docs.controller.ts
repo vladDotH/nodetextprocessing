8cfc93e8a92478b1444af8b7fb004005447d92dc
Vlad <vladbolkunovv@gmail.com>
2024-07-19 23:00:38 +0300

    fix(docs): env prefix

--- src/controllers/docs.controller.ts
+++ src/controllers/docs.controller.ts
@@ -1,8 +1,11 @@
 import Router from "koa-router";
 import { koaSwagger } from "koa2-swagger-ui";
 import specs from "@/swagger/swagger.json";
+import { configService } from "@/services/config.service";
 
-export const docsController = new Router({ prefix: "/docs" });
+export const docsController = new Router({
+  prefix: configService.SWAGGER_PREFIX,
+});
 
 docsController.get(
   "/",
