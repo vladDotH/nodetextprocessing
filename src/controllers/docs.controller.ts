191ead6fa617f5911ccb1667c4864c4bac96bbe0
Vlad <vladbolkunovv@gmail.com>
2024-07-19 23:01:49 +0300

    Revert "fix(docs): env prefix"
    This reverts commit 8cfc93e8a92478b1444af8b7fb004005447d92dc.

--- src/controllers/docs.controller.ts
+++ src/controllers/docs.controller.ts
@@ -1,11 +1,8 @@
 import Router from "koa-router";
 import { koaSwagger } from "koa2-swagger-ui";
 import specs from "@/swagger/swagger.json";
-import { configService } from "@/services/config.service";
 
-export const docsController = new Router({
-  prefix: configService.SWAGGER_PREFIX,
-});
+export const docsController = new Router({ prefix: "/docs" });
 
 docsController.get(
   "/",
